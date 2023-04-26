d7652ca5241cb5ec7633e0324a2cc697925f276d
gloria bello <calderonscarlette69@gmail.com>
2023-04-26 10:17:32 -0400

    lecture-0

---
+++ lecture0/hello.py
@@ -0,0 +1,12 @@
+# pide al usuario su nombre.
+name = input("what's your name? ")
+
+# saludar al usuario
+print("hello,")
+print(name)
+
+# crea tus propias funciones por ejemplo:
+def hello(to):
+    print("hello,",to)
+    name = input("what's your name? ")
+hello(name)
